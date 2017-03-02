970d33b1aca9a742da05bd87750af658da773766
Ryota, Sakaguchi <sakaguchi.ryota@jp.fujitsu.com>
2017-03-02 18:36:32 +0900

    add template

---
+++ README.md
@@ -0,0 +1,40 @@
+Tab-navigation template for monaca
+---
+
+タブによるページ切り替えと、ナビゲーションによるPush/Popの2つの画面遷移を組み合わせたテンプレートです。
+
+# スクリーンショット
+iOS  
+![iOS](docs/ios.png)
+
+Android  
+![Android](docs/android.png)
+
+# 機能
+- タブとナビゲーションの組み合わせ
+- 画面遷移時の値渡し
+- AngularJSのhttp APIを使ったREST API通信
+- Nifty Mobile Backendモジュールの組み込み
+
+# 構成
+画面のヒエラルキー
+
+```
+index.html(Tab)
+│
+│
+├─view/home.html(Navigation)
+│  ├─master.html
+│  ├─detail.html
+│
+└─view/account.html
+```
+
+`index.html`にタブタグが、`view/home.html`にナビゲーションタグがあります。これらを起点として画面遷移が行われます。
+
+Tabは`view/home.html`と、`view/account.html`の表示をスイッチします。Navigationは`view/master.html`と`view/detail.html`の表示を切り替えます。  
+アプリが開いている間、`view/home.html`は画面遷移の状態を保持します。よってdetailを表示している状態でAccountタブに切り替えても、もう一度Homeタブに切り替えた時detailが開かれた状態のままとなります。  
+__この挙動はスマートフォンネイティブアプリの動作と同じため、仕様です。__
+
+# ライセンス
+MIT License.
---
+++ www/app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+ons.bootstrap()
+  .controller('AppController', function() {
+    // Root Controller
+  })
+  .controller('HomeMasterController', function($scope) {
+    // Pass from javascript to html
+    $scope.numbers = number100();
+
+    // Define Event function
+    $scope.showDetail = function(number) {
+      // Push page with params
+      home.nav.pushPage('view/home/detail.html', { data: { num: number } });
+    };
+    
+
+    function number100() {
+      // faster than new Array[100]
+      const box = [];
+      for (var i=1; i <= 100; i++) {
+        box.push(i);
+      }
+      return box;
+    }
+  })
+  .controller('HomeDetailController', function($scope) {
+    // Get the params from pushed page
+    const extra = home.nav.topPage.data;
+    $scope.num = extra.num;
+    
+    // Dialog function
+    $scope.showDialog = function(message) {
+      navigator.notification.alert(message);
+    };
+  })
+  .controller('AccountController', function($scope, $http) {
+    // Get the OnsenUI GitHub
+    $http.get('https://api.github.com/users/OnsenUI').then(function(response) {
+      if (response.status === 200) {
+        $scope.onsenui = response.data;
+      } else {
+        alert('Error http: ' + response.status);
+      }
+    });
+  });
+  
+  
+ons.ready(function() {
+  // Nifty Mobile Backend(Push)
+  /*
+  window.NCMB.monaca.setDeviceToken(
+    "", // Nifty application key
+    "", // Nifty client key
+    ""  // Firebase ID
+  );
+  window.NCMB.monaca.setReceiptStatus(true);
+  */
+});
